c98ffd1ee9c8657c03a6fad1d173a394e99bc340
MeowVal <meowval@catkatnya.com>
2026-03-27 06:17:14 +0100

    initial path tracer technique

---
+++ src/core/src/render_techniques/fast_path_tracer/fast_path_tracer.h
@@ -0,0 +1,148 @@
+/**********************************************************************
+Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+********************************************************************/
+#pragma once
+
+#include "../../ray_tracing/path_tracing_shared.h"
+#include "render_technique.h"
+
+#include <gfx_scene.h>
+
+namespace Capsaicin
+{
+class FastPT : public RenderTechnique
+{
+public:
+    FastPT();
+    ~FastPT() override;
+
+    FastPT(FastPT const &other)                = delete;
+    FastPT(FastPT &&other) noexcept            = delete;
+    FastPT &operator=(FastPT const &other)     = delete;
+    FastPT &operator=(FastPT &&other) noexcept = delete;
+
+    /*
+     * Gets configuration options for current technique.
+     * @return A list of all valid configuration options.
+     */
+    RenderOptionList getRenderOptions() noexcept override;
+
+    struct RenderOptions
+    {
+        uint32_t fast_pt_bounce_count = 30; /**< Maximum number of bounces each path can take */
+        uint32_t fast_pt_min_rr_bounces =
+            2; /**< Number of bounces a path takes before Russian roulette can be used */
+        uint32_t fast_pt_sample_count = 1; /**< Number of paths to trace per pixel per frame */
+        bool     fast_pt_disable_albedo_materials =
+            false; /**< Sets material to fixed diffuse gray on first intersected surface */
+        bool fast_pt_disable_direct_lighting =
+            false; /**< Disable sampling direct lighting on first intersection */
+        bool fast_pt_disable_specular_materials =
+            false; /**< Disable specular sampling/evaluation essentially setting materials to diffuse only */
+        bool fast_pt_disable_alpha_testing =
+            false; /**< Disable testing material alpha - treats all surfaces as opaque */
+        bool fast_pt_nee_only    = false; /**< Disable light contributions from source other than NEE */
+        bool fast_pt_disable_nee = false; /**< Disable light contributions from Next Event Estimation */
+        bool fast_pt_use_dxr10   = false; /**< Use dxr 1.0 ray-tracing pipelines instead of inline rt */
+        bool fast_pt_accumulate  = true;  /**< Enable accumulation of frames */
+    };
+
+    /**
+     * Convert render options to internal options format.
+     * @param options Current render options.
+     * @return The options converted.
+     */
+    static RenderOptions convertOptions(RenderOptionList const &options) noexcept;
+
+    /**
+     * Gets a list of any shared components used by the current render technique.
+     * @return A list of all supported components.
+     */
+    [[nodiscard]] ComponentList getComponents() const noexcept override;
+
+    /**
+     * Gets the required list of shared textures needed for the current render technique.
+     * @return A list of all required shared textures.
+     */
+    [[nodiscard]] SharedTextureList getSharedTextures() const noexcept override;
+
+    /**
+     * Initialise any internal data or state.
+     * @note This is automatically called by the framework after construction and should be used to create
+     * any required CPU|GPU resources.
+     * @param capsaicin Current framework context.
+     * @return True if initialisation succeeded, False otherwise.
+     */
+    bool init(CapsaicinInternal const &capsaicin) noexcept override;
+
+    /**
+     * Perform render operations.
+     * @param [in,out] capsaicin The current capsaicin context.
+     */
+    void render(CapsaicinInternal &capsaicin) noexcept override;
+
+    /**
+     * Destroy any used internal resources and shutdown.
+     */
+    void terminate() noexcept override;
+
+    /**
+     * Render GUI options.
+     * @param [in,out] capsaicin The current capsaicin context.
+     */
+    void renderGUI(CapsaicinInternal &capsaicin) const noexcept override;
+
+protected:
+    /**
+     * Initialise internal computer kernels.
+     * @param capsaicin The current capsaicin context.
+     * @return True if initialisation succeeded, False otherwise.
+     */
+    bool initKernels(CapsaicinInternal const &capsaicin) noexcept;
+
+    /**
+     * Check if kernels needs to be recompiled.
+     * @param capsaicin The current capsaicin context.
+     * @param newOptions New render options.
+     * @return True if kernels needs to be recompiled, False otherwise.
+     */
+    [[nodiscard]] virtual bool needsRecompile(
+        CapsaicinInternal const &capsaicin, RenderOptions const &newOptions) const noexcept;
+
+    virtual void setupSbt(CapsaicinInternal const &capsaicin) const noexcept;
+
+    virtual void setupPTKernel(CapsaicinInternal const &capsaicin,
+        std::vector<GfxLocalRootSignatureAssociation>  &local_root_signature_associations,
+        std::vector<std::string> &defines, std::vector<std::string> &exports,
+        std::vector<std::string> &subobjects) noexcept;
+
+    virtual char const *getProgramName() noexcept;
+
+    GfxBuffer  rayCameraData;
+    GfxTexture accumulationBuffer; /**< Buffer used to store pixel running average, .w= number of samples */
+    RayCamera  cameraData;
+    RenderOptions options;
+
+    GfxProgram fast_pt_program_;
+    GfxKernel  fast_pt_kernel_;
+    GfxSbt     fast_pt_sbt_;
+};
+} // namespace Capsaicin
